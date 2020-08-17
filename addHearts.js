201ec9c262d68bba7a312f32a14a2351387f0a54
Cole Manning // Rareform <45409688+RVRX@users.noreply.github.com>
2020-08-17 13:44:57 -0400

    Update addHearts.js

--- addHearts.js
+++ addHearts.js
@@ -1,15 +1,28 @@
-let unhearted = [];
+function getUnhearted() {
 
-var hearts = document.getElementsByClassName('heart-follow');
+	//get all hearts on page
+	var hearts = document.getElementsByClassName('heart-follow');
 
-for(var i = 0; i < hearts.length; i++) {
-	// if heart is not filled
-	if (hearts[i].getElementsByClassName("heart unfilled")) {
-		unhearted.push(hearts[i])
+	//add all un-hearted listings to array
+	let unhearted = [];
+	for(var i = 0; i < hearts.length; i++) {
+		// if heart is not filled
+		if (hearts[i].getElementsByClassName("heart unfilled")) {
+			unhearted.push(hearts[i])
+		}
+	}
+	return unhearted;
+}
+
+function clickAllElements(arrayOfElements) {
+	for (var i = arrayOfElements.length - 1; i >= 0; i--) {
+		arrayOfElements[i].click()
+		console.log("Clicked", arrayOfElements[i])
 	}
 }
 
-// heart all unhearted elements
-for (var i = unhearted.length - 1; i >= 0; i--) {
-	unhearted[i].click()
-}
+//run the functions
+setTimeout(function() {
+	clickAllElements(getUnhearted())
+  	location.reload();
+}, 30000);
